9e306e7fe5c1f6663742df2761b88e7d0c085d4b
ins1x <ins16mail@gmail.com>
2023-06-26 18:57:46 +0300

    upload new version
    Add color table, add object render

--- AbsEventHelper.lua
+++ AbsEventHelper.lua
@@ -13,17 +13,20 @@ local memory = require 'memory'
 local encoding = require 'encoding'
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
-
+font = renderCreateFont("Arial", 8, 5)
+ 
 local sizeX, sizeY = getScreenResolution()
 local main_window_state = imgui.ImBool(false)
 local show_favorites = imgui.ImBool(false)
 local show_credits = imgui.ImBool(false)
 local show_hotkeys = imgui.ImBool(false)
+local show_colors = imgui.ImBool(false)
 local show_worldlimits = imgui.ImBool(false)
 local show_effects = imgui.ImBool(false)
 local checkbox_chatfilter = imgui.ImBool(true)
 local checkbox_antiafk = imgui.ImBool(true)
 local checkbox_keybinds = imgui.ImBool(true)
+local checkbox_showobjects = imgui.ImBool(false)
 local color = imgui.ImFloat4(1, 0, 0, 1)
 local antiafk = true
 local chatfilter = true
@@ -31,6 +34,7 @@ local keybinds = true
 local effects = true
 local fps = 0
 local fps_counter = 0
+local showobjects = false
 
 local function starts_with(str, start)
    return str:sub(1, #start) == start
@@ -39,23 +43,18 @@ end
 -- imgui
 function imgui.OnDrawFrame()
    if main_window_state.v then
-      imgui.SetNextWindowSize(imgui.ImVec2(500, 400), imgui.Cond.FirstUseEver)
+      imgui.SetNextWindowSize(imgui.ImVec2(440, 400), imgui.Cond.FirstUseEver)
 	  imgui.SetNextWindowPos(imgui.ImVec2(sizeX / 2, sizeY / 2),
 	  imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
       imgui.Begin("Absolute Events Helper", main_window_state)
-	  
-	  if imgui.Button(u8"Избранные") then
-		 show_favorites.v = not show_favorites.v
-	  end
 	 
-      imgui.SameLine()
 	  if imgui.Button(u8"Лимиты") then
 		 show_worldlimits.v = not show_worldlimits.v
 	  end
 	  
 	  imgui.SameLine()
-	  if imgui.Button(u8"Горячие клавиши") then
-		 show_hotkeys.v = not show_hotkeys.v
+	  if imgui.Button(u8"Цвета") then
+		 show_colors.v = not show_colors.v
 	  end
 	  
 	  imgui.SameLine()
@@ -63,6 +62,11 @@ function imgui.OnDrawFrame()
 		 show_effects.v = not show_effects.v
 	  end
 	  
+	  imgui.SameLine()
+	  if imgui.Button(u8"Горячие клавиши") then
+		 show_hotkeys.v = not show_hotkeys.v
+	  end
+		
 	  imgui.SameLine()
 	  if imgui.Button(u8"О скрипте") then
 		 show_credits.v = not show_credits.v
@@ -93,10 +97,16 @@ function imgui.OnDrawFrame()
 	  imgui.Text(string.format(u8"Ближайший транспорт: %i", closestcarid))
 	  imgui.Separator()
 	  
-	  imgui.ColorEdit4('Color Picker', color)
+	  --imgui.Text(u8"Объекты")
+	  if imgui.Button(u8"Избранные объекты") then
+		 show_favorites.v = not show_favorites.v
+	  end
+	  
 	  imgui.SameLine()
-	  imgui.Text('HEX: '..intToHex(join_argb(color.v[4] * 255, color.v[1] * 255, color.v[2] * 255, color.v[3] * 255)))
-	  imgui.Separator()
+	  imgui.Checkbox(u8("Показывать ID объектов рядом"), checkbox_showobjects)
+	  if checkbox_showobjects.v then
+	     showobjects = not showobjects
+	  end
 	  
 	  imgui.Checkbox(u8("Фильтр подключений в чате"), checkbox_chatfilter)
 	  if checkbox_chatfilter.v then
@@ -129,7 +139,7 @@ function imgui.OnDrawFrame()
 		  -- end
 		  --end
 	  end
-		
+	  
       imgui.End()
    end
    
@@ -151,6 +161,30 @@ function imgui.OnDrawFrame()
       imgui.End()
 	end
 	
+	if show_colors.v then
+	   imgui.SetNextWindowPos(imgui.ImVec2(sizeX / 4, sizeY / 4),
+	   imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+	  
+       imgui.Begin(u8"Цветовая палитра", show_colors)
+       imgui.TextColoredRGB("{FF0000}RED    {FF0000}FF0000")
+       imgui.TextColoredRGB("{008000}GREEN  {008000}008000")
+	   imgui.TextColoredRGB("{0000FF}BLUE   {0000FF}0000FF")
+	   imgui.TextColoredRGB("{FFFF00}YELLOW {FFFF00}FFFF00")
+	   imgui.TextColoredRGB("{FF00FF}PINK   {FF00FF}FF00FF")
+	   imgui.TextColoredRGB("{00ffff}AQUA   {00ffff}00FFFF")
+	   imgui.TextColoredRGB("{00ff00}LIME   {00ff00}00FF00")
+	   imgui.TextColoredRGB("{800080}PURPLE {800080}800080")
+	   imgui.TextColoredRGB("{FFFFFF}WHITE  {FFFFFF}FFFFFF")
+	   imgui.TextColoredRGB("{808080}GREY   {808080}808080")
+	   imgui.TextColoredRGB("{363636}BLACK  {363636}000000")
+	   imgui.ColorEdit4("", color)
+	   imgui.SameLine()
+	   imgui.Text("HEX: " ..intToHex(join_argb(color.v[4] * 255, color.v[1] * 255,
+	   color.v[2] * 255, color.v[3] * 255)))
+	   imgui.Separator()
+	   imgui.End()
+	end
+	
 	if show_hotkeys.v then
 	  imgui.SetNextWindowPos(imgui.ImVec2(sizeX / 4, sizeY / 4),
 	  imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
@@ -162,7 +196,12 @@ function imgui.OnDrawFrame()
       imgui.Text(u8"M - домашний транспорт")
       imgui.Text(u8"K - заказать транспорт")
       imgui.Text(u8"H - перевернуть транспорт")
-	  
+	  imgui.Separator()
+	  imgui.Text(u8"Если у вас установлен Samp Addon вы можете отключить горячие клавиши")
+	  imgui.Checkbox(u8("Горячие клавиши"), checkbox_keybinds)
+	  if checkbox_keybinds.v then
+	     keybinds = not keybinds
+	  end
 	  -- local test_text_buffer = imgui.ImBuffer(256)
 	  -- if imgui.InputText(u8"Вводить текст сюда", test_text_buffer) then 
 		 -- u8:decode(test_text_buffer.v)
@@ -177,6 +216,13 @@ function imgui.OnDrawFrame()
 	  imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
 	  
       imgui.Begin(u8"Лимиты", show_worldlimits)
+	  imgui.Text(u8"Каждый игрок от 20 уровня может при наличии свободных слотов создать свой мир для строительства.")
+	  imgui.Text(u8"Если все слоты уже были заняты, вы можете воспользоваться любым открытым миром.")
+	  imgui.Text(u8"Для создания мира необходимо иметь 100 ОА (Очков апгрейда) и 1.000.000$.")
+      imgui.Text(u8"По умолчанию в мире можно создавать только 50 объектов, лимит можно расширить до 300.")imgui.Text(u8"VIP игроки могут расширять лимит до 2000 объектов.")
+	  imgui.Text(u8"Стоимость расширения мира 20 ОА и 500.000$ за 10 объектов.") 
+	  imgui.Text(u8"Максимальное количество созданных миров 500. При отсутствии на сервере 90 дней мир удаляется")
+	  imgui.Separator()
       imgui.Text(u8"Лимиты в мире")
       imgui.Text(u8"макс. объектов: 300 (VIP 2000)")
       imgui.Text(u8"макс. объектов в одной точке: 200 ")
@@ -185,6 +231,9 @@ function imgui.OnDrawFrame()
       imgui.Text(u8"макс. транспорта: 50")
       imgui.Text(u8"макс. слотов под гонки: 5")
       imgui.Text(u8"макс. виртуальных миров: 500")
+	  imgui.Separator()
+	  imgui.Text(u8"В радиусе 150 метров нельзя создавать более 200 объектов.")
+	  imgui.Text(u8"Максимальная длина текста на объектах в редакторе миров - 50 символов")
       imgui.End()
 	end
 	
@@ -225,7 +274,7 @@ function imgui.OnDrawFrame()
       imgui.Text(u8"Помошник для мапперов и организаторов мероприятий на Absolute DM")
       imgui.Text(u8"Homepage: Русскоязычное сообщество мапперов: vk.com\1nsanemapping")
       imgui.Text(u8"Credits:")
-      imgui.Text(u8"FYP - SAMP.lua library")
+      imgui.Text(u8"FYP - imgui and SAMP lua library")
       imgui.Text(u8"MOL - antiafk")
       imgui.Text(u8"Gorskin - useful memory hacks")
       imgui.End()
@@ -239,6 +288,9 @@ function main()
       sampAddChatMessage("" .. tag, 0xFFFFFF)
 	  while true do
 	  wait(0)
+	  
+	  -- Imgui menu
+	  imgui.Process = main_window_state.v
 	  
 	  -- chatfilter
 	  function sampev.onServerMessage(color, text)
@@ -292,8 +344,18 @@ function main()
          main_window_state.v = not main_window_state.v 
 	  end)
 	  
-      imgui.Process = main_window_state.v
-	end
+	  if showobjects then
+	     for _, v in pairs(getAllObjects()) do
+		    if isObjectOnScreen(v) then
+			   local _, x, y, z = getObjectCoordinates(v)
+			   local x1, y1 = convert3DCoordsToScreen(x,y,z)
+			   local model = getObjectModel(v)
+			   renderFontDrawText(font, "{80FFFFFF}" .. model, x1, y1, -1)
+			end
+		 end
+	  end
+		
+   end
 end
 
 function direction()
@@ -378,4 +440,56 @@ end
 
 function intToHex(int)
     return '{'..string.sub(bit.tohex(int), 3, 8)..'}'
+end
+
+function imgui.TextColoredRGB(text)
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    local ImVec4 = imgui.ImVec4
+
+    local explode_argb = function(argb)
+        local a = bit.band(bit.rshift(argb, 24), 0xFF)
+        local r = bit.band(bit.rshift(argb, 16), 0xFF)
+        local g = bit.band(bit.rshift(argb, 8), 0xFF)
+        local b = bit.band(argb, 0xFF)
+        return a, r, g, b
+    end
+
+    local getcolor = function(color)
+        if color:sub(1, 6):upper() == 'SSSSSS' then
+            local r, g, b = colors[1].x, colors[1].y, colors[1].z
+            local a = tonumber(color:sub(7, 8), 16) or colors[1].w * 255
+            return ImVec4(r, g, b, a / 255)
+        end
+        local color = type(color) == 'string' and tonumber(color, 16) or color
+        if type(color) ~= 'number' then return end
+        local r, g, b, a = explode_argb(color)
+        return imgui.ImColor(r, g, b, a):GetVec4()
+    end
+
+    local render_text = function(text_)
+        for w in text_:gmatch('[^\r\n]+') do
+            local text, colors_, m = {}, {}, 1
+            w = w:gsub('{(......)}', '{%1FF}')
+            while w:find('{........}') do
+                local n, k = w:find('{........}')
+                local color = getcolor(w:sub(n + 1, k - 1))
+                if color then
+                    text[#text], text[#text + 1] = w:sub(m, n - 1), w:sub(k + 1, #w)
+                    colors_[#colors_ + 1] = color
+                    m = n
+                end
+                w = w:sub(1, n - 1) .. w:sub(k + 1, #w)
+            end
+            if text[0] then
+                for i = 0, #text do
+                    imgui.TextColored(colors_[i] or colors[1], u8(text[i]))
+                    imgui.SameLine(nil, 0)
+                end
+                imgui.NewLine()
+            else imgui.Text(u8(w)) end
+        end
+    end
+
+    render_text(text)
 end
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Lua script Assistant for mappers and event makers on [Absolute Play DM](https://vk.com/absdm) server  
 The script partially restores the functionality of the samp addon [Samp Addon](https://sa-mp.ru/sampaddon)  
-Find more at https://vk.com/1nsanemapping  
+Find more at [forum.gta-samp.ru](https://forum.gta-samp.ru/index.php?/topic/1016832-%D0%BC%D0%B8%D1%80%D1%8B-%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B-%D1%80%D0%B5%D0%B4%D0%B0%D0%BA%D1%82%D0%BE%D1%80%D0%B0-%D0%BA%D0%B0%D1%80%D1%82/)  
 
 > Its a demo version, minor bugs and shortcomings are possible
 
